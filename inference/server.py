bc00fbfda000646b7d6075c8c71a748b83135567
Eupham <57550307+Eupham@users.noreply.github.com>
2026-03-17 15:59:39 -0400

    Update server.py

--- inference/server.py
+++ inference/server.py
@@ -5,18 +5,15 @@ from inference.utils import get_tier_config, build_llama_image, BaseInference
 _cfg = get_tier_config("moderate")
 
 @app.cls(
-    image=build_llama_image(_cfg["model"], _cfg["filename"]),
+    image=build_llama_image(**_cfg),
     gpu=_cfg.get("gpu", "L40S"),
     scaledown_window=_cfg.get("scaledown_window", 2),
     timeout=_cfg.get("timeout", 1800),
 )
 class Inference(BaseInference):
-    def __init__(self):
-        super().__init__(_cfg)
-
     @modal.enter()
     def start(self):
-        self.start_logic()
+        self.start_logic(_cfg)
 
     @modal.method()
     def generate(self, **kwargs):
