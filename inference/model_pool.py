c70db30e3195496f86c09ecc98172a554938abb2
Eupham <57550307+Eupham@users.noreply.github.com>
2026-03-18 00:01:40 -0400

    Add mounts to inference tier configurations

--- inference/model_pool.py
+++ inference/model_pool.py
@@ -6,7 +6,10 @@ from inference.utils import get_tier_config, build_llama_image, BaseInference
 _cfg_small = get_tier_config("trivial")
 @app.cls(
     image=build_llama_image(**_cfg_small), 
-    gpu="T4", scaledown_window=2, timeout=600
+    gpu="T4", 
+    scaledown_window=2, 
+    timeout=600,
+    mounts=[modal.Mount.from_local_python_packages("orchestrator")]
 )
 class InferenceSmall(BaseInference):
     @modal.enter()
@@ -19,7 +22,10 @@ class InferenceSmall(BaseInference):
 _cfg_medium = get_tier_config("simple")
 @app.cls(
     image=build_llama_image(**_cfg_medium), 
-    gpu="L4", scaledown_window=2, timeout=600
+    gpu="L4", 
+    scaledown_window=2, 
+    timeout=600,
+    mounts=[modal.Mount.from_local_python_packages("orchestrator")]
 )
 class InferenceMedium(BaseInference):
     @modal.enter()
@@ -32,7 +38,10 @@ class InferenceMedium(BaseInference):
 _cfg_large = get_tier_config("expert")
 @app.cls(
     image=build_llama_image(**_cfg_large), 
-    gpu="L40S", scaledown_window=2, timeout=1800
+    gpu="L40S", 
+    scaledown_window=2, 
+    timeout=1800,
+    mounts=[modal.Mount.from_local_python_packages("orchestrator")]
 )
 class InferenceLarge(BaseInference):
     @modal.enter()
